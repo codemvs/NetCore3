7ddf79d6786a8dd3c149e39ac044a09bb0883e6c
codemvs <codemvs@gmail.com>
2020-06-27 00:37:48 -0500

    Se agrega proyecto inicial PeliculasAPI. Configuración EntityFramework, AutoMapper, CRUD Genero

---
+++ PeliculasAPI/PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenerosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GenerosController(ApplicationDbContext context,
+            IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GeneroDTO>>> Get()
+        {
+            var entidades = await context.Generos.ToListAsync();
+            var dtos = mapper.Map<List<GeneroDTO>>(entidades);
+            return dtos;
+        }
+        [HttpGet("{id}", Name = "obtenerGenero")]
+        public async Task<ActionResult<GeneroDTO>> Get(int id)
+        {
+            var genero = await context.Generos.FirstOrDefaultAsync(x => x.Id == id);
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            var dto = mapper.Map<GeneroDTO>(genero);
+            return dto;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] GeneroCreacionDTO generoCreacionDTO)
+        {
+            var genero = mapper.Map<Genero>(generoCreacionDTO);
+            context.Add(genero);
+            await context.SaveChangesAsync();
+            var generoDTO = mapper.Map<GeneroDTO>(genero);
+            return new CreatedAtRouteResult("obtenerGenero", new { id = generoDTO.Id }, generoDTO);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] GeneroCreacionDTO generoCreacionDTO)
+        {
+            var genero = mapper.Map<Genero>(generoCreacionDTO);
+            genero.Id = id;
+            context.Entry(genero).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Generos.AnyAsync(x => x.Id == id);
+            if(!existe)
+            {
+                return NotFound();
+            }
+            context.Generos.Remove(new Genero() { Id = id});
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ PeliculasAPI/PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Helpers
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            // Genero < - > GeneroDTO
+            CreateMap<Genero, GeneroDTO>().ReverseMap();
+            // GeneroCreacionDTO -> Genero
+            CreateMap<GeneroCreacionDTO, Genero>();
+        }
+    }
+}
